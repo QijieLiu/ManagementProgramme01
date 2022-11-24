80420a5ed1efab448b9693e82999acbd66b8d3d1
Liu <814853350@qq.com>
2022-11-24 18:31:56 +0800

    Initial commit

---
+++ src/main/java/com/manage/ManagementProgrammeApplication.java
@@ -0,0 +1,15 @@
+package com.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementProgrammeApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ManagementProgrammeApplication.class, args);
+    }
+
+}
